5e57c18c8e68b3ef5835a74a8b290ee9d98695fb
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-02-03 11:44:07 +0000

    feat(ui): implement basic ui
    Implemented basic UI, including a title, canvas, dummy
    control panel, and a footer.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Footer from "./components/Footer";
 
 export default function App() {
     return (
-        <h1>Hello World!</h1>
+        <div className="container">
+            <h1>Navigation app</h1>
+            <canvas className="canvas" />
+            <div className="control-panel">
+                <button>Button 1</button>
+                <button>Button 2</button>
+            </div>
+            <Footer />
+        </div>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Developed and Maintained by C. Dempsey, 2023</p>
+        </footer>
+    )
+}
